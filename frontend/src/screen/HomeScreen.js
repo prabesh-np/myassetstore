92d6622ba0f2dc6e45e5976ae7fc2b0982b31882
prabesh-np <prabeshthapa.npofficial@gmail.com>
2021-12-30 10:10:02 +0545

    intro to redux reducers

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-   const [products, setProducts] = useState([]);
+   const dispatch = useDispatch();
+
    useEffect(() => {
-      const fetchProducts = async () => {
-         const { data } = await axios.get("/api/products");
-         setProducts(data);
-      };
-      fetchProducts();
-   }, []);
+      dispatch(listProducts());
+   }, [dispatch]);
+
+   const products = [];
 
    return (
       <>
